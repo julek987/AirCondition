b6a6ef246f257bbb57bbf9e7c50095db0e6cbef4
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-04-06 14:07:30 +0100

    Initial commit

---
+++ src/main/java/com/example/tqs_116726_hw1/Tqs116726Hw1Application.java
@@ -0,0 +1,13 @@
+package com.example.tqs_116726_hw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tqs116726Hw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tqs116726Hw1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tqs_116726_hw1/Tqs116726Hw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tqs_116726_hw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tqs116726Hw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
